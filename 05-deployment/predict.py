20af33b28f6295d7538ac6a8c0e29bc33dd3c1c1
xlepotato <yucia181@gmail.com>
2024-10-30 11:26:12 +0800

    Update 05-deploy.ipynb

--- 05-deployment/predict.py
+++ 05-deployment/predict.py
@@ -1,37 +1,30 @@
 # Load the model
 import pickle
+from flask import Flask, request, jsonify
 
 input_file = 'model_C=1.0.bin'
 
 with open(input_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-customer = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
+app = Flask('churn')
 
-# Turn this customer into feature matrix
-X = dv.transform([customer])
-# Get the probability that this particular customer is going to churn
-y_pred = model.predict_proba(X)[0, 1]
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
 
-print('Input', customer)
-print('Churn probability', y_pred)
+    # Turn this customer into feature matrix
+    X = dv.transform([customer])
+    # Get the probability that this particular customer is going to churn
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+
+    result = {
+        'churn_probability': float(y_pred),
+        'churn': bool(churn)
+    }
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
